47e169a90d4c763c95bbd78a9b705d9c9eadd3c3
Matthew Baseman <matthew.baseman@gmail.com>
2023-04-26 11:04:30 -0700

    First run at Mongo setup

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,31 @@
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+mongoose.connect('mongodb://localhost/glossary');
+
 // 2. Set up any schema and models needed by the app
+let glossarySchema = mongoose.Schema({
+  // Schema goes here
+  word: { type: String, unique: true },
+  description: String
+})
+
+let Glossary = mongoose.model('Glossary', glossarySchema);
+
+let save = (newWord) => {
+  return Glossary.create(newWord);
+}
+
+let getAll = () => {
+  return Glossary.find({})
+    .sort('-word')
+    .exec()
+}
+
 // 3. Export the models
+module.exports.save = save;
+module.exports.getAll = getAll
+
 // 4. Import the models into any modules that need them
+
+
